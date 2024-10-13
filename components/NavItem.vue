defa05453d1685730198cb9b09cdcf32ba67df08
Marcin Bojara <marcin_5@mail.ru>
2024-10-13 15:07:36 +0200

    Add NavItem component for navigation links
    This commit introduces a new `NavItem.vue` component to manage individual navigation links. The component is designed to be flexible and includes dynamic classes based on the current item's state and properties. This addition aims to enhance navigation and improve code modularity.

---
+++ components/NavItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li>
+    <NuxtLink
+      :to="item.link"
+      :class="[
+        'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent',
+        { 'text-white bg-blue-700 md:bg-transparent md:text-blue-700': item.current },
+      ]"
+      :aria-current="item.current ? 'page' : undefined"
+    >
+      {{ item.text }}
+    </NuxtLink>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
