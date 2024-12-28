ef999b572fa25fe882abc025482d58919b04cd17
Marcin Bojara <marcin_5@mail.ru>
2024-12-28 09:50:27 +0100

    Refactor: define specific URL constant for now-playing movies
    Replaced the generic API base URL with a dedicated constant for the now-playing movies endpoint. This improves code readability and makes the URL usage more explicit and maintainable.

--- server/api/movies/nowPlaying.js
+++ server/api/movies/nowPlaying.js
@@ -1,10 +1,10 @@
-const API_BASE_URL = 'https://api.themoviedb.org/3'
+const NOW_PLAYING_MOVIES_URL = 'https://api.themoviedb.org/3/movie/now_playing'
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig(event)
   const { AccessToken } = config
 
-  const nowPlayingMovies = await $fetch(API_BASE_URL + '/movie/now_playing', {
+  const nowPlayingMovies = await $fetch(NOW_PLAYING_MOVIES_URL, {
     method: 'GET',
     headers: {
       accept: 'application/json',
