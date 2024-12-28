0996388dc916238b64b8644ab4eca377d6f9756f
Marcin Bojara <marcin_5@mail.ru>
2024-12-28 09:34:33 +0100

    Add top-rated movies and TV series sections
    Introduced sections for displaying top-rated movies and TV series, utilizing `MovieCard` and `SeriesCard` components. Data is fetched from the `/api/movies/topRated` endpoint and populated into reactive state variables.

--- pages/top-rated.vue
+++ pages/top-rated.vue
@@ -1,5 +1,58 @@
 <template>
-  <div></div>
+  <div>
+    <section>
+      <h1
+        class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+      >
+        Top Rated Movies
+      </h1>
+
+      <ul class="grid grid-cols-5 gap">
+        <li v-for="movie in movies">
+          <MovieCard
+            :poster_path="movie.poster_path"
+            :title="movie.title"
+            :id="movie.id"
+            :overview="movie.overview"
+            :release_date="movie.release_date"
+            :popularity="movie.popularity"
+            :item="movie"
+          />
+        </li>
+        <br /><br />
+      </ul>
+    </section>
+    <section>
+      <h1
+        class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+      >
+        Top Rated Tv Series
+      </h1>
+
+      <ul class="grid grid-cols-5 gap">
+        <li v-for="tvSeries in series">
+          <SeriesCard
+            :poster_path="tvSeries.poster_path"
+            :name="tvSeries.name"
+            :overview="tvSeries.overview"
+            :first_air_date="tvSeries.first_air_date"
+            :popularity="tvSeries.popularity"
+            :item="tvSeries"
+          />
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const movies = useState(() => [])
+const series = useState(() => [])
+
+const { data } = await useFetch('/api/movies/topRated', {
+  transform: (data) => {
+    movies.value = data.topRatedMovies.results
+    series.value = data.topRatedSeries.results
+  },
+})
+</script>
