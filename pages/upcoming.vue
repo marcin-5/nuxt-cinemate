79c545ebbea10f82f23038c760e23605b16c2e53
Marcin Bojara <marcin_5@mail.ru>
2024-12-28 09:54:01 +0100

    Add upcoming movies list with dynamic MovieCard components
    Introduced a new section to display upcoming movies using a grid layout. Fetched data dynamically from the `/api/movies/upcoming` endpoint and passed it to the `MovieCard` components. This enhances the page by providing users with an overview of upcoming movie releases.

--- pages/upcoming.vue
+++ pages/upcoming.vue
@@ -1,5 +1,36 @@
 <template>
-  <div></div>
+  <div>
+    <section>
+      <h1
+        class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+      >
+        Upcoming Movies
+      </h1>
+
+      <ul class="grid grid-cols-5 gap">
+        <li v-for="movie in movies">
+          <MovieCard
+            :poster_path="movie.poster_path"
+            :title="movie.title"
+            :id="movie.id"
+            :overview="movie.overview"
+            :release_date="movie.release_date"
+            :popularity="movie.popularity"
+            :item="movie"
+          />
+        </li>
+        <br /><br />
+      </ul>
+    </section>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const movies = useState(() => [])
+
+const { data } = await useFetch('/api/movies/upcoming', {
+  transform: (data) => {
+    movies.value = data.upcomingMovies.results
+  },
+})
+</script>
