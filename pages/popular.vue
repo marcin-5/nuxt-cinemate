5cebd5298df5b3811a3ca577e09a2192e29b7b88
Marcin Bojara <marcin_5@mail.ru>
2024-12-28 17:33:25 +0100

    Add error handling with ErrorAlert across movie pages
    Integrated the ErrorAlert component to handle errors on popular, top-rated, now-playing, and upcoming movie pages. This ensures users are informed of issues when data fetching fails, improving the user experience.

--- pages/popular.vue
+++ pages/popular.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <ErrorAlert v-if="error" :error="error" />
+  <div v-else>
     <section>
       <h1
         class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
@@ -29,6 +30,7 @@
 
 <script setup>
 import { fetchData } from '/composables/fetchData.js'
+import ErrorAlert from '/components/ErrorAlert.vue'
 
 const { movies, series, error } = await fetchData('/api/movies/popular', 'popularMovies', 'popularSeries')
 </script>
