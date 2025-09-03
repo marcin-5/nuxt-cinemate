372d7d3869c9928bf4faf7f3b94803f14cc30def
Marcin Bojara <marcin_5@mail.ru>
2025-09-03 10:43:05 +0200

    Update styles to replace blue with gray in NavItem and search input
    Replaced Tailwind's blue color palette with gray for hover, focus, and active states in NavItem and search input components. Aligns with the updated design guidelines for a neutral aesthetic.

--- components/NavItem.vue
+++ components/NavItem.vue
@@ -3,8 +3,8 @@
     <NuxtLink
       :to="item.link"
       :class="[
-        'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent',
-        { 'text-white bg-blue-700 md:bg-transparent md:text-blue-700': item.current },
+        'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 dark:text-white md:dark:hover:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent',
+        { 'text-white bg-gray-700 md:bg-transparent md:text-gray-700': item.current },
       ]"
       :aria-current="item.current ? 'page' : undefined"
     >
--- pages/search.vue
+++ pages/search.vue
@@ -24,7 +24,7 @@
           v-model="searchTerm"
           type="text"
           id="default-search"
-          class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
           placeholder="Search Movies, Series..."
           required
         />
